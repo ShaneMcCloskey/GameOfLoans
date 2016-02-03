5b78d8904b13a625b9c76f57426a071a884ef879
Abarn279 <Abarn279@gmail.com>
2016-02-03 18:01:40 -0500

    nav controller update
    …

--- GameOfLoansUnity/Assets/Scripts/NavigationController.cs
+++ GameOfLoansUnity/Assets/Scripts/NavigationController.cs
@@ -5,13 +5,14 @@ using UnityEngine.UI;
 
 public class NavigationController : MonoBehaviour 
 {
-	public Text propertyName;
-
 	public GameObject mainMenuPanel;
+	public GameObject howToPlayPanel;
+	public GameObject leaderboardsPanel;
+	public GameObject creditsPanel;
 	public GameObject homePanel;
-	public GameObject phPanel;
-	public GameObject lipPanel;
-	public GameObject okPanel;
+	public GameObject propertyHuntPanel;
+	public GameObject loanInProgressPanel;
+	public GameObject oppKnocksPanel;
 	public GameObject HUDPanel;
 
 	public GameController gameControler;
@@ -23,53 +24,57 @@ public class NavigationController : MonoBehaviour
 	{
 		currentPanel = mainMenuPanel;
 		mainMenuPanel.SetActive (true);
+		howToPlayPanel.SetActive (false);
+		leaderboardsPanel.SetActive (false);
+		creditsPanel.SetActive (false);
 		homePanel.SetActive(false);
-		phPanel.SetActive (false);
-		lipPanel.SetActive (false);
-		okPanel.SetActive (false);
+		propertyHuntPanel.SetActive (false);
+		loanInProgressPanel.SetActive (false);
+		oppKnocksPanel.SetActive (false);
 		HUDPanel.SetActive (false);
 	}
 
 	// main menu buttons
 	public void OnButtonPlay()
 	{
-		ChangePanel (homePanel);
+		ChangePanel (homePanel, true);
 	}
 
 	public void OnButtonHowToPlay()
 	{
+		ChangePanel (howToPlayPanel, false);
 	}
 
 	public void OnButtonLeaderboards()
 	{
+		ChangePanel (leaderboardsPanel, false);
 	}
 
 	public void OnButtonCredits()
 	{
+		ChangePanel (creditsPanel, false);
 	}
 
 	// Home button in all panels
 	public void OnButtonHome()
 	{
-		currentPanel.SetActive (false);
-		homePanel.SetActive (true);
-		currentPanel = homePanel;
+		ChangePanel (homePanel, true);
 	}
 		
 	// Home buttons
 	public void OnButtonOppKnocks()
 	{
-		currentPanel.SetActive (false);
-		okPanel.SetActive (true);
-		currentPanel = okPanel;
+		ChangePanel (oppKnocksPanel, true);
 	}
 
 	public void OnButtonPropertyHunt()
 	{
+		ChangePanel (propertyHuntPanel, true);
 	}
 
 	public void OnButtonLoanInProgress()
 	{
+		ChangePanel (loanInProgressPanel, true);
 	}
 
 	// Opp knocks buttons
@@ -78,8 +83,16 @@ public class NavigationController : MonoBehaviour
 		gameControler.DrawOppKnocksCard();
 	}
 
-	void ChangePanel(GameObject panelToShow)
+	void ChangePanel(GameObject panelToShow, bool showHUD)
 	{
+		if (showHUD) 
+		{	
+			HUDPanel.SetActive (true);
+		} 
+		else 
+		{
+			HUDPanel.SetActive (false);	
+		}
 		currentPanel.SetActive (false);
 		panelToShow.SetActive (true);
 		currentPanel = panelToShow;
