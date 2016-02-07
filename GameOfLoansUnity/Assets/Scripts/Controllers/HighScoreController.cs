5bd2a0fb0f94576b6e9a3ca3dd0c84ebe18e873d
Tim Taviano <tavianot@gmail.com>
2016-02-07 17:48:59 -0500

    Trying to get the high scores table to pull the json
    For some reason the built in son parser doesn’t want to work

--- GameOfLoansUnity/Assets/Scripts/Controllers/HighScoreController.cs
+++ GameOfLoansUnity/Assets/Scripts/Controllers/HighScoreController.cs
@@ -1,7 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Runtime.Serialization;
+
 
 public class HighScoreController : MonoBehaviour {
 	public Player player;
+	public Text textbox;
+
+	public string getscores = "http://35.9.22.106/Api/HighScores/GetHighScores?n=4";
+
+	public void Start(){
+		WWW www = new WWW (getscores);
+		StartCoroutine (WaitRequestGet (www));
+	}
+
+	private IEnumerator WaitRequestGet(WWW www){
+	
+		yield return www;
+		GetScores (www);
+	}
+
+	public void GetScores(WWW www)
+	{
+		 
+		ArrayOfGameScores data = JsonUtility.FromJson<ArrayOfGameScores> (www.text);
+
+		textbox.text = data.ArrayOfGameScore[0].Name;
+
+	}
+		
+
 
 }
---
+++ GameOfLoansUnity/Assets/Scripts/HighScoreObjects/GameScore.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+
+public class GameScore  {
+	public int Id;
+	public int LoansClosed;
+	public string Name;
+	public string TeamName;
+	public int Score;
+
+}
