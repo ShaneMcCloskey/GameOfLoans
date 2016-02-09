5495a374469c7012edfc23511d9d0e9c28be6fb6
Tim Taviano <tavianot@gmail.com>
2016-02-09 13:23:16 -0500

    High scores functionality for the first page

--- GameOfLoansUnity/Assets/Scripts/Controllers/HighScoreController.cs
+++ GameOfLoansUnity/Assets/Scripts/Controllers/HighScoreController.cs
@@ -6,9 +6,15 @@ using UnityEngine.UI;
 using Newtonsoft.Json;
 
 
+
 public class HighScoreController : MonoBehaviour {
 	public Player player;
-	public Text textbox;
+	public GameObject scoresPanel;
+	const int RANK_LOC = 0;
+	const int NAME_LOC = 1;
+	const int TEAM_LOC = 2;
+	const int Score_LOC = 3;
+	const int LOANS_LOC = 4;
 
 	public string getscores = "http://35.9.22.106/Api/HighScores/GetHighScores?n=4";
 
@@ -20,12 +26,32 @@ public class HighScoreController : MonoBehaviour {
 	private IEnumerator WaitRequestGet(WWW www){
 	
 		yield return www;
-		GetScores (www);
+		SetScores (www);
 	}
 
-	public IEnumerable GetScores(WWW www)
+	public void SetScores(WWW www)
 	{
-		return JsonConvert.DeserializeObject<List<GameScore>>(www.text);
+		List<GameScore> data = JsonConvert.DeserializeObject<List<GameScore>>(www.text);
+		int count = scoresPanel.transform.childCount;
+		for(int i=0;i<25;i++)
+		{
+			// if not enough data
+			if (i >= data.Count)
+			{
+				break;
+			}
+			//set the rank
+			scoresPanel.transform.GetChild(i).GetChild(RANK_LOC).GetChild(0).GetComponent<Text>().text = (i+1).ToString();
+			//set the name
+			scoresPanel.transform.GetChild(i).GetChild(NAME_LOC).GetChild(0).GetComponent<Text>().text = data[i].Name;
+			//set the team name
+			scoresPanel.transform.GetChild(i).GetChild(TEAM_LOC).GetChild (0).GetComponent<Text>().text = data[i].TeamName;
+			//set Score
+			scoresPanel.transform.GetChild(i).GetChild(Score_LOC).GetChild (0).GetComponent<Text>().text = data[i].Score.ToString();
+			//set Loans closed
+			scoresPanel.transform.GetChild(i).GetChild(LOANS_LOC).GetChild (0).GetComponent<Text>().text = data[i].LoansClosed.ToString();
+			
+		}
 	}
 		
 
